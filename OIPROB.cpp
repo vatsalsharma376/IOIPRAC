d2243b7deabba5b640b9de8eaa0921a3ae457d63
githubba123 <32459834+githubba123@users.noreply.github.com>
2017-10-02 15:12:05 +0000

    Create OIPROB.cpp

---
+++ OIPROB.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    
+    int m, n;
+    cin>>m>>n;
+    n--;
+    vector< vector<int> > v(n, vector<int>(m));
+    for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+            cin>>v[j][i];
+    vector<int> pre(n);
+    pre[0] = *max_element(v[0].begin(),v[0].end());
+    for(int i=1;i<n;i++)
+        pre[i] = pre[i-1]+*max_element(v[i].begin(),v[i].end());
+    int ans = 0;
+    for(int i = 1;i<n;i++)
+        for(int j = 0;j<i;j++)
+            ans = max(ans,pre[i]-pre[j]);
+    
+    cout<<max(ans,*max_element(pre.begin(),pre.end()))<<endl;
+    
+    return 0;
+}
